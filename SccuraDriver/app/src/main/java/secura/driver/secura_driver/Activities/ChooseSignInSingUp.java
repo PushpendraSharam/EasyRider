eca5854a50875b4eb3857de698954e840446fce0
Pushpendra <azadsharma343@gmail.com>
2023-06-13 15:04:52 +0530

    New commit

---
+++ SccuraDriver/app/src/main/java/secura/driver/secura_driver/Activities/ChooseSignInSingUp.java
@@ -0,0 +1,16 @@
+package secura.driver.secura_driver.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import secura.driver.secura_driver.R;
+
+public class ChooseSignInSingUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_sign_in_sing_up);
+    }
+}
