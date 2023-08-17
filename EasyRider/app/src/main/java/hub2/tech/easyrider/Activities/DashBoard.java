6798b5c57ac4f6ab1e8048215daedf675fda96bd
Pushpendra <azadsharma343@gmail.com>
2023-08-17 10:41:44 +0530

    This is updated on 17-08-2023

--- EasyRider/app/src/main/java/hub2/tech/easyrider/Activities/DashBoard.java
+++ EasyRider/app/src/main/java/hub2/tech/easyrider/Activities/DashBoard.java
@@ -1,16 +1,31 @@
 package hub2.tech.easyrider.Activities;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import hub2.tech.easyrider.BottomSheetPickUp;
 import hub2.tech.easyrider.R;
+import hub2.tech.easyrider.databinding.ActivityDashBoardBinding;
 
 public class DashBoard extends AppCompatActivity {
+    ActionBarDrawerToggle toggle;
+    ActivityDashBoardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dash_board);
+        binding = ActivityDashBoardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Displaying Navigation bar
+        toggle = new ActionBarDrawerToggle(this, binding.drawerLayout,binding.toolbar, R.string.open, R.string.close);
+        binding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //Opening Bottom sheet
+        BottomSheetPickUp bSheet=new BottomSheetPickUp();
+        bSheet.show(getSupportFragmentManager(),bSheet.getTag());
     }
 }
